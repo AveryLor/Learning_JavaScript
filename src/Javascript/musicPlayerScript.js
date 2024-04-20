11ec16b7df4280c76f8f9cd3d5bd1522f6d49c9f
Avery Lor <averywlor@gmail.com>
2024-04-20 15:06:15 -0400

    Fixed some formatting issues and the overall script for the music player project.

--- src/Javascript/musicPlayerScript.js
+++ src/Javascript/musicPlayerScript.js
@@ -257,10 +257,31 @@ audio.addEventListener("ended", () => {
     if (nextSongExists) {
       playNextSong();
     } else {
-      userData.currentSong = null;
-      userData.songCurrentTime = 0;  
-      pauseSong(); 
-setPlayerDisplay(); 
-highlightCurrentSong(); 
-setPlayButtonAccessibleText(); 
-      
+        userData.currentSong = null;
+        userData.songCurrentTime = 0;  
+        pauseSong(); 
+        setPlayerDisplay(); 
+        highlightCurrentSong(); 
+        setPlayButtonAccessibleText(); 
+}
+
+});
+
+const sortSongs = () => {
+  userData?.songs.sort((a,b) => {
+    if (a.title < b.title) {
+      return -1;
+    }
+
+    if (a.title > b.title) {
+      return 1;
+    }
+
+    return 0;
+  });
+
+  return userData?.songs;
+};
+
+renderSongs(sortSongs());
+setPlayButtonAccessibleText();

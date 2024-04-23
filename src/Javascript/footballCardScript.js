0bd2931603fae7f0dbba28254af6ae3572029ae5
Avery Lor <averywlor@gmail.com>
2024-04-22 23:14:42 -0400

    Finished footballCard script.

--- src/Javascript/footballCardScript.js
+++ src/Javascript/footballCardScript.js
@@ -221,3 +221,9 @@ playersDropdownList.addEventListener("change", (e) => {
         players.filter((player) => player.position === "goalkeeper")
       );
       break;
+      default:
+    setPlayerCards();
+    break;
+}
+});
+
